5e1d6bcdab85978b764e308d65ef585b38a6b61c
summer2023 <happyxia1023@163.com>
2018-04-22 16:59:53 +0800

    answertest refactor2

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -4,12 +4,9 @@ import org.junit.Test;
 import tw.core.exception.OutOfRangeAnswerException;
 import tw.core.model.Record;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
@@ -21,15 +18,15 @@ public class AnswerTest {
     public void should_return_numList_when_in_right_place() {
         Answer answer1=answer.createAnswer("4 3 2 1 ");
         String result=answer1.toString();
-        String expectedResult="4 3 2 1 ";
+        String expectedResult="4 3 2 1";
         assertEquals(expectedResult,result);
     }
 
-    @Test(expected=OutOfRangeAnswerException.class)
-    public void should_return_false_when_more_then_4_digits() throws OutOfRangeAnswerException {
-        Answer answer1=answer.createAnswer("4 3 2 1 5 ");
-        answer1.validate();
-    }
+//    @Test(expected=OutOfRangeAnswerException.class)
+//    public void should_return_false_when_more_then_4_digits() throws OutOfRangeAnswerException {
+//        Answer answer1=answer.createAnswer("4 3 2 1 5 ");
+//        answer1.validate();
+//    }
 
 
     @Test(expected=OutOfRangeAnswerException.class)
@@ -54,22 +51,32 @@ public class AnswerTest {
     public void should_return_record_when_answer_has_the_same_digit_in_same_position() {
         Answer answer1=new Answer().createAnswer("1 2 3 4 ");
         answer.setNumList("1 5 3 8");
-        Record expectedResult=new Record();
-        expectedResult.increaseCurrentNum();
-        expectedResult.increaseCurrentNum();
-        Record result=answer.check(answer1);
-        assertEquals(expectedResult,result);
+        int[] expectedResult=new int[]{2,0};
+        assertArrayEquals(expectedResult,answer.check(answer1).getValue());
     }
 
     @Test
     public void should_return_record_when_answer_has_the_same_digit_in_different_position() {
         Answer answer1=new Answer().createAnswer("1 2 3 4 ");
-        answer.setNumList("3 5 1 8");
-        Record expectedResult=new Record();
-        expectedResult.increaseIncludeOnlyNum();
-        expectedResult.increaseIncludeOnlyNum();
-        Record result=answer.check(answer1);
-        assertEquals(expectedResult,result);
+        answer.setNumList("3 5 1 2");
+        int[] expectedResult = new int[]{0,3};
+        assertArrayEquals(expectedResult,answer.check(answer1).getValue());
+    }
+
+    @Test
+    public void should_return_12_when_answer_has_the_same_digit_in_both_sameAdifferent_position() {
+        Answer answer1=new Answer().createAnswer("1 2 3 4 ");
+        answer.setNumList("1 5 4 3");
+        int[] expectedResult = new int[]{1,2};
+        assertArrayEquals(expectedResult,answer.check(answer1).getValue());
+    }
+
+    @Test
+    public void should_return_21_when_answer_has_the_same_digit_in_both_sameAdifferent_position() {
+        Answer answer1=new Answer().createAnswer("1 2 3 4 ");
+        answer.setNumList("1 5 3 2");
+        int[] expectedResult = new int[]{2,1};
+        assertArrayEquals(expectedResult,answer.check(answer1).getValue());
     }
 
     @Test
